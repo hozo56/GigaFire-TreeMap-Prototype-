a4b604a5aacb17d5832747bd3df68ecfa415e3bf
Taylor R Brown <84751477+hozo56@users.noreply.github.com>
2022-06-09 16:50:45 -0700

    Add files via upload

--- fia_conversion_sql_to_csv.R
+++ fia_conversion_sql_to_csv.R
@@ -12,7 +12,48 @@ library(RSQLite)
 # Before running the code below download SQLite db for a particular state from
 # the Datamart & unzip:
 # - https://apps.fs.usda.gov/fia/datamart/datamart_sqlite.html
-# - This script uses Colorado as an example.
+# - This script uses CA as an example.
+
+#setwd("R:/SCRATCH/taylorrbrown/gigafire_treelist/github_treelist/SQLiteconversionFIAtables/sql_states_db")
+
+########list all files in the directory
+all_states_db <- list.files()
+path_out <- "R:\\SCRATCH\\taylorrbrown\\gigafire_treelist\\github_treelist\\SQLiteconversionFIAtables\\csv\\"
+
+#####function to convert all us states fia sqlite into csv  for Mongodb
+
+sql_to_csv_fia <- function(db_file){
+  # Take a reference to the db (again, using CA as the example)
+  con <- dbConnect(RSQLite::SQLite(), db_file)
+  state_abrev <- substr(x=db_file,start=7,stop=8)
+  # grab all the table names
+  # - alternatively, subset this to only those tables you want
+  db_table_names <- c("COND","SUBPLOT", "PLOT")
+  #db_table_names <- dbListTables(con)
+  
+  # iterate through the table names and write out a csv for each table in the db
+  lapply(db_table_names, function(x) {
+    write.csv(dbReadTable(conn = con,x),
+              file = paste0(path_out, state_abrev,"_", x, ".csv"))
+  })
+  
+  # close connection
+  dbDisconnect(con)
+}
+
+lapply(all_states_db,sql_to_csv_fia)
+
+
+
+
+
+
+
+
+
+
+###################original way to convert sqlite by FIA 
+
 
 # Set path to where the SQLite is located
 path_db <- "R:\\SCRATCH\\taylorrbrown\\gigafire_treelist\\github_treelist\\SQLiteconversionFIAtables\\SQLite_FIADB_CA\\"
